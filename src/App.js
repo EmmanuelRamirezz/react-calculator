fda49586ff94a77d3067cabe894042fae8c114b9
EmmanuelRamirezz <emmanuelrub_@hotmail.com>
2023-03-11 14:44:46 -0600

    Estructura principal creada. Creando funciones

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
+import calcLogo from './assets/calcLogo.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='calc-logo--container'>
+      <img
+      src={calcLogo}
+      className='calc-logo'
+      alt = 'Calculatos logo' />
+      </div>
+
+      <div className='calculator-container'>
+        <div className='row'></div>
+        <div className='row'></div>
+        <div className='row'></div>
+        <div className='row'></div>
+      </div>
     </div>
   );
 }
